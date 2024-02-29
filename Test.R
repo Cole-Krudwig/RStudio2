389e233e748aa1b05db2fd870beeb056de0cb3d2
Cole Krudwig <ckrudwig@gmail.com>
2024-02-29 11:12:01 -0600

    Connection to WRDS

--- Test.R
+++ Test.R
@@ -1,1 +1,10 @@
-# Test Commit
+wrds <- dbConnect(Postgres(),
+                  host='wrds-pgdata.wharton.upenn.edu',
+                  port=9737,
+                  dbname='wrds',
+                  sslmode='require',
+                  user='colekrudwig')
+
+res <- dbSendQuery(wrds,"SELECT date,dji FROM djones.djdaily")
+data <- dbFetch(res, n = -1)
+plot(as.Date(data$date,'%Y-%m-%d'),data$dji,xlab="date",ylab="dji",type='l',col='red')
